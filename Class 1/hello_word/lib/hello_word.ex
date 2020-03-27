b34ca64711abb2de70b0c5cdb2fd2bdcad334a12
David Fabian <david.fabian@phinxlab.com>
2020-03-26 22:57:34 -0300

    class 1 completed

---
+++ Class 1/hello_word/lib/hello_word.ex
@@ -0,0 +1,18 @@
+defmodule HelloWord do
+  @moduledoc """
+  Documentation for `HelloWord`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloWord.hello()
+      "Hello Word"
+
+  """
+  def hello do
+    "Hello Word"
+  end
+end
